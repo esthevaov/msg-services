43fbf2dc0d03f911978f16d1c5b8143cea5c563c
unknown <esthevao.vervloet@gmail.com>
2018-12-29 11:44:20 -0200

    user reset email and password

--- src/main/java/com/vervloet/msgservices/presentation/UserController.java
+++ src/main/java/com/vervloet/msgservices/presentation/UserController.java
@@ -1,11 +1,12 @@
 package com.vervloet.msgservices.presentation;
 
 import com.vervloet.msgservices.domain.User;
-import com.vervloet.msgservices.persistence.UserRepository;
+import com.vervloet.msgservices.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -13,13 +14,32 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
+
+    @GetMapping()
+    public List<User> getAllUsers(){
+
+        return userService.getAllUsers();
+
+    }
 
     // Create a new Message
-    @PostMapping("")
+    @PostMapping()
     public User createUser(@Valid @RequestBody User user) {
 
-        return userRepository.save(user);
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}/change-email")
+    public User updateUserEmail(@RequestBody User user,
+                                @PathVariable(value = "id") Long userId){
+        return userService.updateUserEmail(user, userId);
+    }
+
+    @PutMapping("/{id}/change-password")
+    public User updateUserPassword(@RequestBody Map<String, String> passwords,
+                                   @PathVariable(value = "id") Long userId){
+        return userService.updateUserPassword(passwords, userId);
     }
 
 }
